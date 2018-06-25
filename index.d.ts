5f36a49a86b749832dcc73ccaf1907fa9cd1379f
Volkan Nazmi Metin <v.metin@triodor.eu>
2018-06-25 10:12:01 +0300

    Fix for type definitions

--- index.d.ts
+++ index.d.ts
@@ -1,20 +1,18 @@
 import mongoose from "mongoose";
 
-type MongoModel = mongoose.Model<T>;
-const mc = modelConvertor;
+type MongoModel = mongoose.Model<any>;
 
 declare namespace mongo {
-	declare const Schema = mongoose.Schema;
-	declare const mongoConnectionState = {
-		disconnected: 0,
-		connected: 1,
-		connecting: 2,
-		disconnecting: 3
-	};
-	declare function connect(mongodbConnection: string): Promise<boolean>;
-	declare function model(name: stirng, schema: mongoose.Schema): MongoModel;
-	declare function save(mongomodel: MongoModel, modelconvertor: (entity) => MongoModel, selector: (entity) => Object): (entities) => Promise<any>;
-	declare function remove(mongomodel: MongoModel): (any?: Object) => Promise<boolean>;
-	declare function fetch(mongomodel: MongoModel, domainConvertor: (model: MongoModel) => Object): (any?: Object) => Promise<T>;
-	declare function disconnect(): Promise<void>;
+	enum mongoConnectionState {
+		disconnected = 0,
+		connected = 1,
+		connecting = 2,
+		disconnecting = 3
+	}
+	function connect(mongodbConnection: string): Promise<boolean>;
+	function model(name: string, schema: mongoose.Schema): MongoModel;
+	function save(mongomodel: MongoModel, modelconvertor: (entity) => MongoModel, selector: (entity) => Object): (entities) => Promise<any>;
+	function remove(mongomodel: MongoModel): (any?: Object) => Promise<boolean>;
+	function fetch(mongomodel: MongoModel, domainConvertor: (model: MongoModel) => Object): (any?: Object) => Promise<any>;
+	function disconnect(): Promise<void>;
 }
