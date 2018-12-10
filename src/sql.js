dd45b7f79e49a662f14feb0b580ba4e7a5aaa41c
Volkan Nazmi Metin <volkan.nazmi.metin@gmail.com>
2018-12-10 16:04:43 +0300

    SQL rewrite for pool management

--- src/sql.js
+++ src/sql.js
@@ -2,52 +2,23 @@
 const sql = require('mssql');
 const MAXWAITTIMETOCONNECT = 10000; // Give max 10secs for sql server to open a connection pool
 
-const connect = async pool => {
-	if (!pool.connected && !pool.connecting) {
-		await pool.connect(err => err ? console.log(err) : console.log('Connected to SQL'));
-	}
-	return new Promise((resolve, reject) => {
-		let trialCount = 0;
-		const testConnectionTimer = setInterval(() => {
-			trialCount++;
-			if(pool.connected){
-				clearInterval(testConnectionTimer);
-				resolve(pool.connected);
-			} 
-			if(trialCount > MAXWAITTIMETOCONNECT / 100) {
-				clearInterval(testConnectionTimer);
-				reject('Sql server did not respond connection request for 10 secs');
-			}
-		}, 100);
-	});
-};
-/**
- * Parameters needed to connect mssql server
- */
-class ConnectionParams {
-	/**
-	 * @param {string} user Username to connect mssql server
-	 * @param {string} password Password to connect mssql server
-	 * @param {string} server Mssql server host endpoint
-	 * @param {string} database Database name on mssql server
-	 */
-	constructor(user, password, server, database) {
-		this.user = user;
-		this.password = password;
-		this.server = server;
-		this.database = database;
-	}
-}
 
-let pool = null;
 /**
- * Initialize and configure the mssql server connection pool 
- * @param {ConnectionParams} connectionParams 
+ * @param {string} user Username to connect mssql server
+ * @param {string} password Password to connect mssql server
+ * @param {string} server Mssql server host endpoint
+ * @param {string} database Database name on mssql server
  */
-const init = connectionParams => {
-	if (pool) {
-		return;
-	}
+const ConnectionParams = function (user, password, server, database) {
+	return {
+		user: user,
+		password: password,
+		server: server,
+		database: database
+	};
+};
+
+function initSQLPool(connectionParams) {
 	const poolOptions = Object.assign({
 		options: {
 			encrypt: true
@@ -58,25 +29,56 @@ const init = connectionParams => {
 			idleTimeoutMillis: 30000
 		}
 	}, connectionParams);
-	pool = new sql.ConnectionPool(poolOptions);
-	//pool.on('error', err => console.log(err));
-};
+	return new sql.ConnectionPool(poolOptions);
+}
 
-
-module.exports = {
-	sql: sql,
-	pool: () => pool,
-	ConnectionParams: ConnectionParams,
-	request: () => new sql.Request(pool),
-	connect: async connectionParams => {
-		init(connectionParams);
-		return await connect(pool);
-	},
-	isConnected : () => pool.connected,
-	disconnect: async () => {
-		if (!pool) {
-			return;
+const SQL = function () {
+	return {
+		pool: null,
+		/**
+		 * Connects the sql pool 
+		 * @param {ConnectionParams} connectionParams 
+		 */
+		connect: async function (connectionParams) {
+			if (!this.pool) {
+				this.pool = initSQLPool(connectionParams);
+			}
+			if (!this.pool.connected && !this.pool.connecting) {
+				await this.pool.connect(err => err ? console.log(err) : console.log('Connected to SQL'));
+			}
+			return new Promise((resolve, reject) => {
+				let trialCount = 0;
+				const testConnectionTimer = setInterval(() => {
+					trialCount++;
+					if (this.pool.connected) {
+						clearInterval(testConnectionTimer);
+						resolve(this.pool.connected);
+					}
+					if (trialCount > MAXWAITTIMETOCONNECT / 100) {
+						clearInterval(testConnectionTimer);
+						reject('Sql server did not respond connection request for 10 secs');
+					}
+				}, 100);
+			});
+		},
+		sql: sql,
+		ConnectionParams: ConnectionParams,
+		request: function () {
+			if (!this.pool) {
+				throw new Error('SQL pool not initialized/connected');
+			}
+			return new sql.Request(this.pool);
+		},
+		isConnected: function () {
+			return this.pool ? this.pool.connected : false;
+		},
+		disconnect: function () {
+			if (!this.pool) {
+				return;
+			}
+			this.pool.close();
 		}
-		pool.close();
-	}
+	};
 };
+
+module.exports = SQL();
